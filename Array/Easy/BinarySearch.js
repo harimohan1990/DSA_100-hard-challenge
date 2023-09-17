197d2fc1f67c0da9d336037306ba6dfcf3e35516
harimohan <harimohan.info@gmail.com>
2023-09-17 07:20:57 +0530

    binary search

--- Array/Easy/BinarySearch.js
+++ Array/Easy/BinarySearch.js
@@ -1,4 +1,4 @@
-var search = function(nums, target) {
+var binarySearch = function(nums, target) {
     let lo = 0, hi = nums.length-1;
     while (lo < hi) {
         let mid = lo + Math.floor((hi-lo+1)/2);
@@ -10,6 +10,6 @@ var search = function(nums, target) {
     }
     return nums[lo]==target?lo:-1;
 };
-let x = search([-1,0,3,5,9,12],9);
+let x = binarySearch([-1,0,3,5,9,12],9);
 
 console.log(x)
