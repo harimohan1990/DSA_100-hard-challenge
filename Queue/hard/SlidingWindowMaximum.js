9040ebc9a3d42ed2db62fb58b05d8e5293443bfc
harimohan <harimohan.info@gmail.com>
2023-09-24 12:30:15 +0530

    SlidingWindowMaxium

---
+++ Queue/hard/SlidingWindowMaximum.js
@@ -0,0 +1,33 @@
+
+var maxSlidingWindow = function(nums, k) {
+    const res = [];
+
+    let left = 0;
+
+    let right = 0;
+
+    const q = [];
+
+    while ( right <nums.length){
+        while(q.length >0 && nums[right] > nums[q[q.length-1]]){
+             q.pop();
+        }
+         q.push(right);
+
+        if (left > q[0]) {
+            q.shift();
+        }
+
+        if (right + 1 >= k) {
+            res.push(nums[q[0]]);
+            left++;
+        }
+        right++;
+    }
+
+    return res; 
+    }
+
+    const maxSlidingW = maxSlidingWindow([1,3,-1,-3,5,3,6,7], 3);
+
+    console.log(maxSlidingW)
