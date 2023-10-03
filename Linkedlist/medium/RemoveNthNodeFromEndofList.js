1c0f04933c98d32041b891261a4f6593331503a5
harimohan <harimohan.info@gmail.com>
2023-10-03 15:45:17 +0530

    linkedlist removeNathChild

---
+++ Linkedlist/medium/RemoveNthNodeFromEndofList.js
@@ -0,0 +1,22 @@
+var removeNthFromEnd = function(head, n) {
+    var node = new ListNode()
+    var rep = node
+    var hash = []
+    while(head!==null){
+        hash.push(head.val)
+        head = head.next
+    }
+    var count = hash.length-n
+    let i = 0
+    while(i<hash.length){
+        if(count!==0){
+            node.next = new ListNode(hash[i])
+            count--
+            node = node.next
+        }else{
+            count+=n-1 
+        }
+        i++
+    }
+    return rep.next
+};
