2763e5b3e070b412522d60e57b6d37e72fcc9efa
harimohan <harimohan.info@gmail.com>
2023-10-11 17:57:12 +0530

    string reverseword

---
+++ String/medium/reverseWords.js
@@ -0,0 +1,7 @@
+var reverseWords = function (s) {
+    let str = s.split(" ");
+    let output = "";
+    for (let i = str.length - 1; i >= 0; i--)
+        if (str[i]) output += (output ? " " : "") + str[i];
+    return output;
+};
