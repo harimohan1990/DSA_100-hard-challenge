61fde7c6d2d062823c366e202e44457f4ed5938b
harimohan <harimohan.info@gmail.com>
2023-10-21 17:38:26 +0530

    matrix unique path

---
+++ Matrix/UniquePaths.js
@@ -0,0 +1,22 @@
+var uniquePaths = function(m, n) {
+    
+    let dp = new Array(m).fill().map(() => new Array(n).fill(0));
+ 
+    for (let i = 0; i < m; i++) {
+        dp[i][n-1] = 1;
+    }
+    for (let j = 0; j < n; j++) {
+        dp[m-1][j] = 1;
+    }
+    
+  
+    for (let i = m - 2; i >= 0; i--) {
+        for (let j = n - 2; j >= 0; j--) {
+            dp[i][j] = dp[i+1][j] + dp[i][j+1];
+        }
+    }
+    
+    
+
+    return dp[0][0];
+};
